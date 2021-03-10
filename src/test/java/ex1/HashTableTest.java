77b66c8f1f12523bd4778777c2b8bf54556b4e8e
jmseguraf01 <jmseguraf01@elpuig.xeill.net>
2021-03-10 16:44:58 +0100

    Initial commit

---
+++ src/test/java/ex1/HashTableTest.java
@@ -0,0 +1,31 @@
+package ex1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void count() {
+        HashTable hashTable = new HashTable();
+        Assertions.assertEquals(0, hashTable.count());
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void drop() {
+    }
+}
